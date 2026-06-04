3f5121b3d442787ef7a12ce9e102169461dca65a
Matea Brajdic <matea.brajdic@gmail.com>
2026-06-04 11:22:29 +0200

    feat: add navigation loader for section transitions

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <div className="h-8 w-8 rounded-full border-[3px] border-gray-200 border-t-blue-500 animate-spin" />
+    </div>
+  )
+}
